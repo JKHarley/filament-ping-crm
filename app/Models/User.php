92cd5dc327e35c76b051ec0de2b0bb19e880e169
James Harley <27085725+JKHarley@users.noreply.github.com>
2022-03-31 18:00:39 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -41,4 +42,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Defines what emails can access the Filament Panel
+     *
+     * @return boolean
+     */
+    public function canAccessFilament(): bool
+    {
+        return str_ends_with($this->email, '@example.com') && $this->hasVerifiedEmail();
+    }
 }
